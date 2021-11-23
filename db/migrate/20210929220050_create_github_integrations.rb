864551c7b8838ba33736cdc3da54cc7077b08820
Chaskiel M Grundman <cg2v@andrew.cmu.edu>
2021-11-23 13:32:40 -0500

    Use an oldstyle migration so integer keys work
    Change the version of the github_integrations migration to 5.0 so that
    the users_id column is an int and is compatible with extant user tables

--- db/migrate/20210929220050_create_github_integrations.rb
+++ db/migrate/20210929220050_create_github_integrations.rb
@@ -1,12 +1,16 @@
-class CreateGithubIntegrations < ActiveRecord::Migration[5.2]
+class CreateGithubIntegrations < ActiveRecord::Migration[5.0]
   def change
     create_table :github_integrations do |t|
       t.string :oauth_state
       t.text :access_token_ciphertext
-      t.references :user, foreign_key: true, type: :integer, index: { unique: true }
-      
+      t.references :user, index: { unique: true }
       t.timestamps
     end
     add_index :github_integrations, :oauth_state, unique: true
+    add_foreign_key(
+      :github_integrations,
+      :users,
+      column: :user_id
+    )
   end
 end
